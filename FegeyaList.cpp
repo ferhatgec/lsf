9c44d9c85f2837c5e7bfdaa3bd84544c0ca2245d
ferhatgec <ferhatgectao@gmail.com>
2020-08-04 22:22:57 +0300

    Added first source.

---
+++ FegeyaList.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+#include <FileSystemPlusPlus.h>
+
+void HelpFunction() {
+	std::cout << "Fegeya List\nlsf (--a || --all) : Lists current directory\n";
+}
+
+int main(int argc, char** argv) {
+	if(argc > 1) {
+        	for(int i = 1; i < argc; i++) {
+			std::string arg(argv[i]);
+			if(arg.substr(0, 2) == "--") {
+				if(arg == "--a" || arg ==  "--all") {
+					fsplusplus::List();
+				} else if(arg == "--f" || arg == "--file") {
+					// List only file.
+				} else if(arg == "--h" || arg == "--help") {
+					HelpFunction();
+				} else {
+					HelpFunction();
+				}
+			} else {
+				fsplusplus::List();
+			}
+		}
+        } else {
+        	fsplusplus::List();
+	}
+}
---
+++ make.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+g++ -I./Library/ FegeyaList.cpp -std=c++17 -o lsf
+./lsf --h
